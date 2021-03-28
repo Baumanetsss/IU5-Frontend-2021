7731a64991832413c11291c74a364571ec7353d1
Paul Strikhar <p.strikhar@gmail.com>
2021-03-29 00:45:34 +0300

    lab5 fin

--- lab5/5.js
+++ lab5/5.js
@@ -22,7 +22,28 @@
  */
 
 function makeRoute(arr) {
-    //code here
+    let temp = arr[0]
+    for (i = 0; i < arr.length-1; i++) {
+        for (j = i; j < arr.length; j++) {
+            if (arr[i].from === arr[j].to) {
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+                i = 0;
+                continue
+            }
+        }
+    }
+    return arr
 }
-
+// test = 
+// [
+//     { from: 'L', to: 'M' },
+//     { from: 'M', to: 'N' },
+//     { from: 'A', to: 'L' },
+//     { from: 'B', to: 'A' },
+//     { from: 'N', to: 'Z' }, //LMNABZ
+// ];
+// console.log(makeRoute(test))
+// makeRoute(test)
 module.exports = makeRoute;
